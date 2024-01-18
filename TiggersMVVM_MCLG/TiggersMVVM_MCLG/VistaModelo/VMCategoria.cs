390cc5e96886288e085b3dafb66df95c8f0e1a53
xvelazquezcas <velazquezxana597@gmail.com>
2024-01-18 14:29:19 -0700

    Práctica 11. Ejecutando Trigger

--- TiggersMVVM_MCLG/TiggersMVVM_MCLG/VistaModelo/VMCategoria.cs
+++ TiggersMVVM_MCLG/TiggersMVVM_MCLG/VistaModelo/VMCategoria.cs
@@ -7,6 +7,8 @@ using System.Windows.Input;
 using TrigersMVVM_MCLG.VistaModelo;
 using Xamarin.Forms;
 using TiggersMVVM_MCLG.Modelo;
+using Xamarin.Forms.Internals;
+using System.Linq;
 
 namespace TiggersMVVM_MCLG.VistaModelo
 {
@@ -15,6 +17,8 @@ namespace TiggersMVVM_MCLG.VistaModelo
 		
 		#region VARIABLES
 		string _Texto;
+		bool _activadorAnimacionImg;
+		string _imagen;
 		ObservableCollection<Mcategorias> _listaCategorias;
 		#endregion
 		#region CONSTRUCTOR
@@ -30,9 +34,44 @@ namespace TiggersMVVM_MCLG.VistaModelo
 			get { return _listaCategorias; }
 			set { SetValue(ref _listaCategorias, value); }
 		}
-		#endregion
-		#region PROCESOS
-		public async Task ProcesoAsyncrono()
+        public bool ActivadorAnimacionImg
+        {
+            get { return _activadorAnimacionImg; }
+            set { SetValue(ref _activadorAnimacionImg, value); }
+        }
+        public string Imagen
+        {
+            get { return _imagen; }
+            set { SetValue(ref _imagen, value); }
+        }
+        #endregion
+        #region PROCESOS
+        public void Seleccionar(Mcategorias modelo)
+        {
+			var index = ListaCategorias
+				.ToList()
+				.FindIndex(p => p.descripcion == modelo.descripcion);
+			Imagen = modelo.imagen;
+			if (index > -1)
+			{
+				Deseleccionar();
+				ActivadorAnimacionImg = true;
+				ListaCategorias[index].selected = true;
+				ListaCategorias[index].textColor = "#FFFFFF";
+				ListaCategorias[index].backgroundColor = "#FF506B";
+			}
+        }
+        public void Deseleccionar()
+		{
+			ListaCategorias.ForEach((item) =>
+			{
+				ActivadorAnimacionImg = false;
+				item.selected = false;
+				item.textColor = "#000000";
+				item.backgroundColor = "#EAEDF6";
+			});
+		}
+        public async Task ProcesoAsyncrono()
 		{
 			await DisplayAlert("Titulo", "Mensaje", "Ok");
 		}
@@ -44,7 +83,9 @@ namespace TiggersMVVM_MCLG.VistaModelo
 		#region COMANDOS
 		public ICommand ProcesoAsyncommand => new Command(async () => await ProcesoAsyncrono());
 		public ICommand ProcesoSimpcommand => new Command(MostrarCategorias);
-		#endregion
-	
-	}
+        public ICommand ProcesoSimpSeleccionar => new Command<Mcategorias>((p)=> Seleccionar(p));
+
+        #endregion
+
+    }
 }
